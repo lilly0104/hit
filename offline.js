3cf7de004c4f15781687cc9dcb651b25cf0a649b
lilly0104 <102577603+lilly0104@users.noreply.github.com>
2022-03-29 15:14:19 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1648537248,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/hanner-sheet0.png",
+		"images/hole-sheet0.png",
+		"images/pig-sheet0.png",
+		"images/hit-sheet0.png",
+		"media/wrong.m4a",
+		"media/wrong.ogg",
+		"media/right.m4a",
+		"media/right.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
